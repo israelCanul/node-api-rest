adf6c033580220b97a80cf9efaacfe4c28eb1695
israel <israel.canul@gmail.com>
2015-09-19 00:42:51 -0500

    termino del servidor REST

--- models/tvshow.js
+++ models/tvshow.js
@@ -1,15 +1,18 @@
-var mongooe=require('mongoose');
-var Schema=mongoose.Schema;
+exports = module.exports = function(app, mongoose) {
 
-var tvshowSchema= new Schema({
-	title:{type:String},
-	year:{type:Number},
-	country:{type:String},
-	poster:{type:String},
-	seasons:{type:Number},
-	genre:{type:String,enum:[
-		'Drama','Fantasi','Sci-fi','Thriller','Comedy']},
-	summary:{type:String}	
-});
+	var tvshowSchema = new mongoose.Schema({
+		title: 		{ type: String },
+		year: 		{ type: Number },
+		country: 	{ type: String },
+		poster:  	{ type: String },
+		seasons: 	{ type: Number },
+		genre: 		{
+			type: String,
+			enum: ['Drama', 'Fantasy', 'Sci-Fi', 'Thriller', 'Comedy']
+		},
+		summary: 	{ type: String }
+	});
 
-module.exports = mongoose.model('TVShow',tvshowSchema);
+	mongoose.model('TVShow', tvshowSchema);
+
+};
